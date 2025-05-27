575fa521a62f8d2fb2de7c79c8c99bb8885e6a38
Matías Cruz <matias18233@gmail.com>
2025-05-27 18:25:32 -0300

    Mostrando cuadros con templates

--- src/components/TemplatesList.vue
+++ src/components/TemplatesList.vue
@@ -1,8 +1,31 @@
+<script setup lang="ts">
+  import { onMounted, ref } from 'vue'
+  import { getTemplates } from '../services/templateService'
+
+  const templates = ref([])
+
+  onMounted(async () => {
+    try {
+      const data = await getTemplates()
+      templates.value = data
+      console.log('Templates recibidos:', templates.value)
+    } catch (error) {
+      console.error('Error al cargar templates:', err)
+    }
+  })
+</script>
 <template>
-  <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-    <p class="text-gray-500">Todavía no hay datos para mostrar.</p>
+  <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3"><!--Modificar según tamaño de pantalla-->
+    <div v-if="templates.length === 0" class="text-gray-500">
+      Cargando templates...
+    </div>
+
+    <div v-for="(template, index) in templates" :key="index" class="border p-4 rounded bg-white shadow">
+      <p class="font-bold text-lg">{{ template.name }}</p>
+      <p class="text-sm text-gray-600">Estado: {{ template.status }}</p>
+      <p class="text-sm text-gray-600">Idioma: {{ template.language }}</p>
+    </div>
   </div>
 </template>
-<script setup>
-
-</script>
+<style>
+</style>
---
+++ src/services/templateService.ts
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const API_URL = 'https://www.api.nahuefer.com/api/templates'
+const TENANT_ID = '78cd60c9-e767-4d1e-b45d-6ba74f3742ee'
+
+export async function getTemplates(page = 1, rowsPerPage = 15) {
+    try {
+        const response = await axios.get(API_URL, {
+            headers: {
+                'Accept': 'application/json',
+                'X-tenant': TENANT_ID
+            },
+            params: {
+                page,
+                rows_per_page: rowsPerPage
+            }
+        })
+        return response.data
+    } catch (error) {
+        console.error('Error al obtener templates:', error)
+        throw error
+    }
+}
