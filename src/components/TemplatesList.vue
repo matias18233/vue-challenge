ee5be8b1e7c528d8d1233e446130620d338b3d19
Matías Cruz <matias18233@gmail.com>
2025-05-27 17:07:13 -0300

    Vinculamos Tailwind con el proyecto

---
+++ src/components/TemplatesList.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+    <p class="text-gray-500">Todavía no hay datos para mostrar.</p>
+  </div>
+</template>
+<script setup>
+
+</script>
